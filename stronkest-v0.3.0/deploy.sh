54ddf19e4e310d53d79c3ed8f12fa3257e27fef8
Mirco Kraenz <contact@kraenz.eu>
2025-06-01 21:37:07 +0200

    feat: more monsters

--- stronkest-v0.3.0/deploy.sh
+++ stronkest-v0.3.0/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-USER="YOUR_WINDOWS_USERNAME"
+USER="XMG Win"
 rm -r dist
 npx @dungeonmans-mod-tools/cli validate-refs src
 npx @dungeonmans-mod-tools/cli build src dist/stronkest-v-0-3-0
-rm -r "C:\Users\$USER\AppData\Roaming\Dungeonmans\modcontent\mods\stronkest-v-0-3-0"
-cp -r "dist/stronkest-v-0-3-0" "C:\Users\$USER\AppData\Roaming\Dungeonmans\modcontent\mods\stronkest-v-0-3-0"
+rm -r "C:\Users\\${USER}\AppData\Roaming\Dungeonmans\modcontent\mods\stronkest-v-0-3-0"
+cp -rp "dist/stronkest-v-0-3-0" "C:\Users\\${USER}\AppData\Roaming\Dungeonmans\modcontent\mods\stronkest-v-0-3-0"
 echo 'Deployed'
